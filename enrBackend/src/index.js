a3e967a07067b689c5ff9a3f72cde523ee98b8ea
Mohd Zaid <mohdzaid_se24b02_048@dtu.ac.in>
2026-07-17 18:13:58 +0530

    Add cookie-parser and cors dependencies, update app.js for middleware usage, and create user.controller.js

--- enrBackend/src/index.js
+++ enrBackend/src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    appendFile.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGOdb connection failed!!", err);
+})
